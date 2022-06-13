f93e044eb940b756515b979523b4b9ba4a1ac2e0
Angom <99433451+AngomRanjan@users.noreply.github.com>
2022-06-13 16:34:28 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import newTaskListener from './modules/newTask_handler.js';
-import renderUI from './modules/render_functions.js';
-import delCompleted from './modules/clearAll.js';
-
-renderUI();
-newTaskListener();
-
-document.getElementById('btn-clear').addEventListener('click', delCompleted);
